5c294624e104fa787bfbc67770cb4f5d6a272e78
Juliet <hrishakovayuliia@gmail.com>
2025-08-20 17:20:02 +0200

    finished with authController (login, logout, register functions) + routes

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -4,5 +4,7 @@ import { login, logout, me, register } from '#controllers';
 const authRouter = Router();
 
 authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.delete('/logout', logout);
 
 export default authRouter;
