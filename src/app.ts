4be4d8f231d7e8ceba51d8ec59e0db52366c6caf
Juliet <hrishakovayuliia@gmail.com>
2025-08-20 15:06:32 +0200

    corrected jwt_secret, added access_token_ttl, added jwt_issuer

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import '#db';
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import { authRouter } from '#routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json(), cookieParser());
+
+app.use('/auth', authRouter);
 
 app.listen(port, () =>
   console.log(`\x1b[31mServer listening at http://localhost:${port}\x1b[0m`)
