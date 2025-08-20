24593bcfb973b0639b68e0495e19301c994b3cdb
Juliet <hrishakovayuliia@gmail.com>
2025-08-20 17:53:12 +0200

    implemented middlewares: errorHandler and notFoundHandler

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import '#db';
 import express from 'express';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import { authRouter } from '#routes';
+import { errorHandler, notFoundHandler } from '#middlewares';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +10,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json(), cookieParser());
 
 app.use('/auth', authRouter);
+
+app.use('*splat', notFoundHandler);
+
+app.use(errorHandler);
 
 app.listen(port, () =>
   console.log(`\x1b[31mServer listening at http://localhost:${port}\x1b[0m`)
